9c4a019f54f6263682a513f880ce4065c4dd5a05
Mathieu Capdegelle <mathieu.capdegelle@gmail.com>
2012-09-26 09:28:32 +0200

    initial commit

---
+++ include/short_types.h
@@ -0,0 +1,74 @@
+#ifndef SHORT_TYPES_H
+#define SHORT_TYPES_H
+
+#include "pstdint.h"
+
+typedef uint8_t  U8; 
+typedef  int8_t  I8;
+typedef uint16_t U16;
+typedef  int16_t I16;
+typedef uint32_t U32;
+typedef  int32_t I32;
+typedef uint64_t U64;
+typedef  int64_t I64;
+
+typedef float    F32;
+typedef double   F64;
+
+// endian swapping functions
+
+static U16 swapU16(U16 value)
+{
+  return ((value & 0x00FF) << 8) 
+       | ((value & 0xFF00) >> 8);
+}
+
+static U32 swapU32(U32 value)
+{
+  return ((value & 0x000000FF) << 24)
+       | ((value & 0x0000FF00) << 8)
+       | ((value & 0x00FF0000) >> 8)
+       | ((value & 0xFF000000) >> 24);
+}
+
+static U64 swapU64(U64 value)
+{
+  return ((value & 0x00000000000000FF) << 56)
+       | ((value & 0x000000000000FF00) << 40)
+       | ((value & 0x0000000000FF0000) << 24)
+       | ((value & 0x00000000FF000000) << 8)
+       | ((value & 0x000000FF00000000) >> 8)
+       | ((value & 0x0000FF0000000000) >> 24)
+       | ((value & 0x00FF000000000000) >> 40)
+       | ((value & 0xFF00000000000000) >> 56);
+}
+
+static F32 swapF32(F32 value)
+{
+  union U32F32
+  {
+    U32 m_asU32;
+    F32 m_asF32;
+  } u;
+
+  u.m_asF32 = value;
+  u.m_asU32 = swapU32(u.m_asU32);
+
+  return u.m_asF32;
+}
+
+static F64 swapF64(F64 value)
+{
+  union U64F64
+  {
+    U64 m_asU64;
+    F64 m_asF64;
+  } u;
+
+  u.m_asF64 = value;
+  u.m_asU64 = swapU64(u.m_asU64);
+
+  return u.m_asF64;
+}
+
+#endif // SHORT_TYPES_H
