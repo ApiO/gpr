2af6ba2c2d6886f9787d5a39fd32c5103bb62d1b
Mathieu Capdegelle <mathieu.capdegelle@gmail.com>
2012-10-08 14:53:49 +0200

    Only keep the gangnam-macro-style

--- include/gpr_containers.h
+++ include/gpr_containers.h
@@ -2,6 +2,8 @@
 #define GPR_CONTAINERS_H
 
 //#include "memory.h"
+#include <stdlib.h> 
+#include "gpr_assert.h"
 #include "short_types.h"
 
 // -------------------------------------------------------------------------
@@ -20,55 +22,10 @@ typedef struct
   U16  next;  // index of the next gpr_index_t
 } gpr_index_t;
 
-typedef struct
-{
-  U16  max_items; // maximum number of items available in the table
-  SZ   sz_item;   // size of an item
-  U16  num_items; // number of items in the id lookup table
+#define GPR_INDEX_MASK 0xffff
+#define GPR_ID_ADD 0x10000
 
-  U8  *item_data; // byte array containing items data
-  U32 *item_ids;  // item ids in the same order of item data
-
-  gpr_index_t *indices; // table indices used to avoid item fragmentation
-  U16 freelist_enqueue; // index of gpr_index_t pointing to the previous hole
-  U16 freelist_dequeue; // index of gpr_index_t pointing to the next hole 
-} gpr_idlut_t;
-
-// create an id lookup table allocating memory for "max_items"
-void  gpr_idlut_init   (gpr_idlut_t *table, U16 max_items, SZ sz_item);
-
-// free the id lookup table and all its memory
-void  gpr_idlut_free   (gpr_idlut_t *table);
-
-// add the specified item to the table and return its id
-U32   gpr_idlut_add    (gpr_idlut_t *table, const void *item);
-
-// return 1 or 0 if the table contains or not the item
-I32   gpr_idlut_has    (gpr_idlut_t *table, U32 id);
-
-// return a pointer to the item with the "id" identifier
-void *gpr_idlut_lookup (gpr_idlut_t *table, U32 id);
-
-// remove the item with the "id" identifier (does not free memory)
-void  gpr_idlut_remove (gpr_idlut_t *table, U32 id);
-
-// return the table item list
-void *gpr_idlut_items  (gpr_idlut_t *table);
-
-// return the table item ids
-U32  *gpr_idlut_ids    (gpr_idlut_t *table);
-
-#define INDEX_MASK 0xffff
-#define NEW_ITEM_ID_ADD 0x10000
-
-// -------------------------------------------------------------------------
-// Strongly typed version
-// -------------------------------------------------------------------------
-
-#include <stdlib.h> 
-#include "gpr_assert.h"
-
-#define GPR_IDLUT_DEF(type)                                                 \
+#define GPR_IDLUT_INIT(type)                                                \
 typedef struct                                                              \
 {                                                                           \
   U32  id;                                                                  \
@@ -77,12 +34,13 @@ typedef struct                                                              \
                                                                             \
 typedef struct                                                              \
 {                                                                           \
-  U16   max_items;                                                          \
-  U16   num_items;                                                          \
-  gpr_##type##_idlut_item *items;                                           \
-  gpr_index_t *indices;                                                     \
-  U16 freelist_enqueue;                                                     \
-  U16 freelist_dequeue;                                                     \
+  U16 max_items;        /* maximum number of items available in the table */\
+  U16 num_items;        /* number of items in the id lookup table */        \
+  gpr_##type##_idlut_item                                                   \
+    *items;             /* items of the table */                            \
+  gpr_index_t *indices; /* table indices used to avoid item fragmentation*/ \
+  U16 freelist_enqueue; /* index of the previous available table index */   \
+  U16 freelist_dequeue; /* index of the next available table index */       \
 } gpr_##type##_idlut_t;                                                     \
                                                                             \
 void gpr_##type##_idlut_init(gpr_##type##_idlut_t *table, U16 max_items)    \
@@ -121,7 +79,7 @@ U32 gpr_##type##_idlut_add(gpr_##type##_idlut_t *table,                     \
   gpr_##type##_idlut_item *item;                                            \
   gpr_index_t *in = &table->indices[table->freelist_dequeue];               \
                                                                             \
-  in->id    += NEW_ITEM_ID_ADD;                                             \
+  in->id    += GPR_ID_ADD ;                                                 \
   in->index  = table->num_items;                                            \
                                                                             \
   item = &table->items[in->index];                                          \
@@ -136,26 +94,27 @@ U32 gpr_##type##_idlut_add(gpr_##type##_idlut_t *table,                     \
                                                                             \
 I32 gpr_##type##_idlut_has(gpr_##type##_idlut_t *table, U32 id)             \
 {                                                                           \
-  gpr_index_t *in = &table->indices[id & INDEX_MASK];                       \
+  gpr_index_t *in = &table->indices[id & GPR_INDEX_MASK];                   \
   return in->id == id && in->index != USHRT_MAX;                            \
 }                                                                           \
                                                                             \
 type *gpr_##type##_idlut_lookup(gpr_##type##_idlut_t *table, U32 id)        \
 {                                                                           \
-  return &table->items[table->indices[id & INDEX_MASK].index].value;        \
+  return &table->items[table->indices[id & GPR_INDEX_MASK].index].value;    \
 }                                                                           \
                                                                             \
 void gpr_##type##_idlut_remove(gpr_##type##_idlut_t *table, U32 id)         \
 {                                                                           \
   gpr_##type##_idlut_item *item;                                            \
-  gpr_index_t *in = &table->indices[id & INDEX_MASK];                       \
+  gpr_index_t *in = &table->indices[id & GPR_INDEX_MASK];                   \
                                                                             \
+  /* copy the last item at position of the item to delete to avoid holes */ \
   item = &table->items[in->index];                                          \
   *item = table->items[--table->num_items];                                 \
-  table->indices[item->id & INDEX_MASK].index = in->index;                  \
+  table->indices[item->id & GPR_INDEX_MASK].index = in->index;              \
                                                                             \
   in->index = USHRT_MAX;                                                    \
-	table->freelist_enqueue = id & INDEX_MASK;                                \
+	table->freelist_enqueue = id & GPR_INDEX_MASK;                            \
   if(table->freelist_dequeue == table->max_items)                           \
     table->freelist_dequeue = table->freelist_enqueue;                      \
 }                                                                           \
@@ -165,5 +124,36 @@ gpr_##type##_idlut_items(gpr_##type##_idlut_t *table)                       \
 {                                                                           \
   return table->items;                                                      \
 }                                                                           \
+
+#define gpr_idlut_item(type) gpr_##type##_idlut_item
+#define gpr_idlut_t(type) gpr_##type##_idlut_t
+
+// create an id lookup table allocating memory for "max_items"
+#define gpr_idlut_init(type, table, max_items) \
+  gpr_##type##_idlut_init(table, max_items)
+
+// free the id lookup table and all its memory
+#define gpr_idlut_free(type, table) \
+  gpr_##type##_idlut_free(table)
+
+// add the specified item to the table and return its id
+#define gpr_idlut_add(type, table, item_value) \
+  gpr_##type##_idlut_add(table, item_value)
+
+// return 1 or 0 if the table contains or not the item
+#define gpr_idlut_has(type, table, id) \
+  gpr_##type##_idlut_has(table, id)
+
+// return a pointer to the item with the "id" identifier
+#define gpr_idlut_lookup(type, table, id) \
+  gpr_##type##_idlut_lookup(table, id)
+
+// remove the item with the "id" identifier (does not free memory)
+#define gpr_idlut_remove(type, table, id) \
+  gpr_##type##_idlut_remove(table, id)
+
+// return the table item list
+#define gpr_idlut_items(type, table) \
+  gpr_##type##_idlut_items(table)
 
 #endif // GPR_CONTAINERS_H
